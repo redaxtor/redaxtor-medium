b600903ef2045b6c4177e41a4eb606e662d18e70
Dmitry <zmicerboksha@gmail.com>
2016-05-13 16:20:10 +0300

    colorPicker fix

--- src/extensions/ColorPicker.js
+++ src/extensions/ColorPicker.js
@@ -7,7 +7,7 @@ import vanillaColorPicker from '../helpers/VanillaColorPicker';
         name: "colorPicker",
         action: "applyForeColor",
         aria: "color picker",
-        contentDefault: "<i class='fa fa-pencil editor-color-picker' aria-hidden='true'></i>",
+        contentDefault: "<i class='fa fa-paint-brush editor-color-picker' aria-hidden='true'></i>",
 
         handleClick: function (e) {
             e.preventDefault();
@@ -22,9 +22,6 @@ import vanillaColorPicker from '../helpers/VanillaColorPicker';
 
             // colors for picker
             var pickerColors = [
-                "#1abc9c",
-                "#2ecc71",
-                "#3498db",
                 "#9b59b6",
                 "#34495e",
                 "#16a085",
@@ -37,7 +34,11 @@ import vanillaColorPicker from '../helpers/VanillaColorPicker';
                 "#e74c3c",
                 "#bdc3c7",
                 "#95a5a6",
-                "#f39c12"
+                "#f39c12",
+                "#d2d064",
+                "#4fbbf7",
+                "#ffffff"
+
             ];
 
             var picker = vanillaColorPicker(this.document.querySelector(".medium-editor-toolbar-active .editor-color-picker"));
