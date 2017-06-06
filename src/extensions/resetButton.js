35878e80d9e3f1c52531f7e3e148ab60ad1f6326
AndrewK <andrew@spiralscout.com>
2017-06-06 14:20:59 +0300

    Update icon

--- src/extensions/resetButton.js
+++ src/extensions/resetButton.js
@@ -6,8 +6,8 @@ var MediumEditor = require('medium-editor/dist/js/medium-editor.js');
         init: function () {
             this.button = this.document.createElement('button');
             this.button.classList.add('medium-editor-action', 'reset-button');
-            this.button.innerHTML = '<i class="rx_icon rx_icon-close"></i>';
-            this.button.title = "Reset Changes";
+            this.button.innerHTML = '<i class="rx_icon rx_icon-rewind"></i>';
+            this.button.title = "Reset Current Changes";
             this.handleClickBinded = this.handleClick.bind(this);
             this.resetToHTML = this.base.getContent();
             this.on(this.button, 'click', this.handleClickBinded);
